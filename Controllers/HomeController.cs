5f9a6e14b4c7dd6bae5d17d2b0d1c83b70e756d8
Charbel Mattar <74071943+charbelmattarr@users.noreply.github.com>
2022-07-17 12:12:29 +0300

    Add files via upload

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MobileStoreWebsiteV1._3.Models;
+using System.Diagnostics;
+
+namespace MobileStoreWebsiteV1._3.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
