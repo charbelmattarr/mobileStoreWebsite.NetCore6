4507b1c30c708734d2f91cb77a5348b5a6638ec6
Charbel Mattar <74071943+charbelmattarr@users.noreply.github.com>
2022-07-17 12:13:30 +0300

    Add files via upload

---
+++ Models/Model.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace MobileStoreWebsiteV1._3.Models
+{
+    public partial class Model
+    {
+        public Model()
+        {
+            Products = new HashSet<Product>();
+        }
+
+        public int ModelId { get; set; }
+        public string? ModelName { get; set; }
+        public string? ModelVersion { get; set; }
+
+        public virtual ICollection<Product> Products { get; set; }
+    }
+}
---
+++ Models/Order.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace MobileStoreWebsiteV1._3.Models
+{
+    public partial class Order
+    {
+        public int OrderId { get; set; }
+        public string? OrderDate { get; set; }
+        public string? OrderBalance { get; set; }
+        public string? OrderStreet { get; set; }
+        public string? OrderCity { get; set; }
+        public string? OrderBuilding { get; set; }
+        public string? OrderStatus { get; set; }
+        public string? OrderDeliveredData { get; set; }
+        public int? ProductId { get; set; }
+        public int? UserId { get; set; }
+
+        public virtual Product? Product { get; set; }
+        public virtual User? User { get; set; }
+    }
+}
