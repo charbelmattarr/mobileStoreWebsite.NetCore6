732291827442134653a12150c5464bea3ceea0c0
Charbel Mattar <74071943+charbelmattarr@users.noreply.github.com>
2022-07-17 12:12:48 +0300

    Add files via upload

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using MobileStoreWebsiteV1._3.Models;
+
+namespace MobileStoreWebsiteV1._3.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+        public DbSet<MobileStoreWebsiteV1._3.Models.Model>? Model { get; set; }
+        public DbSet<MobileStoreWebsiteV1._3.Models.Order>? Order { get; set; }
+        public DbSet<MobileStoreWebsiteV1._3.Models.Product>? Product { get; set; }
+        public DbSet<MobileStoreWebsiteV1._3.Models.User>? User { get; set; }
+    }
+}
